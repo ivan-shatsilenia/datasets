f8ca58f989a5d6bc09308b7a36f18d0f5ea6f39c
Ivan Shatsilenia <shatsilenia@gmail.com>
2021-04-22 01:19:04 -0500

    Minor refactor of test_lending_club.py

--- tests/test_lending_club.py
+++ tests/test_lending_club.py
@@ -1,6 +1,7 @@
 from datasets.lending_club import LendingClub
 
-a = LendingClub(root='../data', accepted=True, partition="train")[:]
+root = '/Users/iv7/data'
+a = LendingClub(root=root, accepted=True, partition="train")[:]
 
 print("Accpted applications:")
 print(f"X_numeric.shape: {a['X_numeric'].shape}")
@@ -8,6 +9,6 @@ print(f"X_categorical.shape: {a['X_categorical'].shape}")
 print(f"y.shape: {a['target'].shape}")
 
 print("Rejected applications:")
-r = LendingClub(root='../data', accepted=False, partition="train")[:]
+r = LendingClub(root=root, accepted=False, partition="train")[:]
 print(f"X_numeric.shape: {r['X_numeric'].shape}")
 print(f"X_categorical.shape: {r['X_categorical'].shape}")
